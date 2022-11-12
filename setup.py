53e0aa73ddc7101336974a644a44804efa729d70
Ganesh Lokare <90838133+GaneshLokare@users.noreply.github.com>
2022-11-12 11:38:21 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
 
-    name="dairy",
+    name="Medical Data Extraction",
     version="0.0.1",
     author="Ganesh",
     author_email="lokaregns@gmail.com",
